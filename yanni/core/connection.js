1c8ae3e20716f32a8c0ad463881ce0f9c27322bf
Migs Cabral <migscabral@gmail.com>
2016-05-23 07:29:37 +0800

    Base code for Connection and Neuron classes.

--- yanni/core/connection.js
+++ yanni/core/connection.js
@@ -1,3 +1,15 @@
 class Connection {
-  
+  constructor(fromNeuron, toNeuron, weight) {
+    this.fromNeuron = fromNeuron;
+    this.toNeuron = toNeuron;
+    this.weight = weight;
+  }
+
+  getOutput() {
+    return this.fromNeuron.getOutput();
+  }
+
+  getWeightedOutput() {
+    return this.fromNeuron.getOutput() * weight;
+  }
 }
--- yanni/core/neuron.js
+++ yanni/core/neuron.js
@@ -1,3 +1,25 @@
 class Neuron {
-  
+  constructor(inputConnections, outputConnections, inputFunction, activationFunction) {
+    this.inputConnections = inputConnections;
+    this.outputConnections = outputConnections;
+    this.inputFunction = inputFunction;
+    this.activationFunction = activationFunction;
+    this.netInput = null;
+    this.output = null;
+  }
+
+  activate() {
+    if(typeof this.inputFunction != 'undefined'  && this.inputFunction != null &&
+        typeof this.inputConnections != 'undefined' && Array.isArray(this.inputConnections) &&
+        this.inputConnections.length > 0) {
+      this.netInput = this.inputFunction.getOutput(this.inputConnections);
+
+      this.output = this.activationFunction.getOutput(this.netInput);
+
+      return this.output;
+    }
+
+    return null;
+  }
+
 }
